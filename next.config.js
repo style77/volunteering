6c921d4e03aa77e40a7f2cedf9d38592ec58eb5d
style77 <stylek777@gmail.com>
2022-10-09 16:57:03 +0200

    dodano strone kontaktową

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    apiKey: `${process.env.apiKey}`,
+    authDomain: `${process.env.authDomain}`,
+    projectId: `${process.env.projectId}`,
+    storageBucket: `${process.env.storageBucket}`,
+    messagingSenderId: `${process.env.messagingSenderId}`,
+    appId: `${process.env.appId}`,
+    measurementId: `${process.env.measurementId}`,
+
+    emailJsServiceId: `${process.env.emailJsServiceId}`,
+    emailJsTemplateId: `${process.env.emailJsTemplateId}`,
+    emailJsPublicKey: `${process.env.emailJsPublicKey}`,
+  },
+};
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,14 +1,60 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import { useRef } from "react";
+
+import emailjs from "@emailjs/browser";
+import { Alert, showAlert } from "../components/alert";
 
 const Contact: NextPage = () => {
+  const form: React.RefObject<HTMLFormElement> = useRef(null);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const target = event.target as typeof event.target & {
+      name: { value: string };
+      email: { value: string };
+      message: { value: string };
+      title: { value: string };
+    };
+
+    const data = {
+      name: target.name.value,
+      title: target.title.value,
+      email: target.email.value,
+      message: target.message.value,
+    };
+
+    emailjs
+      .sendForm(
+        `${process.env.emailJsServiceId}`,
+        `${process.env.emailJsTemplateId}`,
+        form.current!,
+        `${process.env.emailJsPublicKey}`
+      )
+      .then(
+        () => {
+          // clear form inputs
+          target.name.value = "";
+          target.title.value = "";
+          target.email.value = "";
+          target.message.value = "";
+
+          showAlert("Wiadomość wysłana! Dziękujemy 💖");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <main className="bg-background-color">
       <Head>
         <title>Volunteering - Kontakt</title>
       </Head>
-      <div className="flex min-h-screen min-w-screen items-center py-2">
-        <div className="flex flex-col basis-1/3 ml-10 mb-56">
+      <div className="flex flex-wrap md:flex-nowrap min-h-screen min-w-screen items-center py-2">
+        <div className="flex flex-col basis-1/3 ml-12 mb-52">
           <h1 className="text-8xl font-inner font-semibold text-main-color">
             Twoje zdanie
           </h1>
@@ -17,8 +63,9 @@ const Contact: NextPage = () => {
             masz propozycję co możemy dodać, pisz śmiało!
           </h6>
         </div>
-        <div className="flex basis-2/3 ml-10">
-          <form>
+        <div className="flex basis-1/3"></div>
+        <div className="flex basis-1/3 ml-10 -mt-32">
+          <form onSubmit={handleSubmit} ref={form}>
             <div className="mb-6">
               <label
                 htmlFor="email"
@@ -26,7 +73,12 @@ const Contact: NextPage = () => {
               >
                 Twój adres e-mail
               </label>
-              <input type="email" id="email" name="email" className="w-full h-11 border-2 border-main-color rounded-md" />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-[18rem] md:w-[32rem] mr-20 h-11 border-2 pl-2 border-main-color rounded-md"
+              />
             </div>
             <div className="mb-6">
               <label
@@ -35,7 +87,12 @@ const Contact: NextPage = () => {
               >
                 Twoje imię
               </label>
-              <input type="text" id="name" name="name" />
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-[18rem] md:w-[32rem] mr-20 h-11 border-2 pl-2 border-main-color rounded-md"
+              />
             </div>
             <div className="mb-6">
               <label
@@ -44,25 +101,39 @@ const Contact: NextPage = () => {
               >
                 Rodzaj zapytania
               </label>
-              <input type="text" id="title" name="title" />
+              <input
+                type="text"
+                id="title"
+                name="title"
+                className="w-[18rem] md:w-[32rem] mr-20 h-11 border-2 pl-2 border-main-color rounded-md"
+              />
             </div>
             <div className="mb-6">
               <label
-                htmlFor="description"
+                htmlFor="message"
                 className="block mb-2 text-md font-inner font-medium text-main-color"
               >
                 Opis
               </label>
               <textarea
-                id="description"
-                name="description"
-                rows="4"
-                cols="50"
+                id="message"
+                name="message"
+                className="w-[18rem] md:w-[32rem] h-52 mr-20 border-2 pl-2 border-main-color rounded-md"
               ></textarea>
+            </div>
+            <div className="mb-8 w-full">
+              <button
+                type="submit"
+                id="submit"
+                className="mr-20 w-36 h-11 border-2 pl-2 bg-main-color text-background-color text-inner text-semibold border-main-color rounded-md"
+              >
+                Wyślij
+              </button>
             </div>
           </form>
         </div>
       </div>
+      <Alert alertId="alert" color="bg-main-color"></Alert>
     </main>
   );
 };
