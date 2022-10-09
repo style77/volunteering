0bf99d0286b9b97e16ec19a066109602ee491411
style77 <stylek777@gmail.com>
2022-10-09 19:24:02 +0200

    poprawiono responsywność dodano podstawowe logowanie, generalne poprawki

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,23 +13,27 @@ const Home: NextPage = () => {
         <title>Volunteering</title>
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center py-2">
-        <div className="flex flex-row gap-48">
-        <div className="h-screen ">
-          <div className="w-1/2 float-left">
-          <p className="font-inner font-semibold text-main-color text-9xl ">
-            Wolontariat
-          </p>
-          <p className="font-inner text-main-color text-4xl">
-            to nie tylko zajęcie, lecz chęć <br /> pomagania społeczeństwu
-          </p>
+        <div className="flex flex-row gap-48 mt-20">
+          <div className="h-screen">
+            <div className="flex flex-col">
+              <h1 className="flex font-inner font-semibold text-transparent bg-clip-text bg-gradient-to-r from-main-color to-main-color-2 text-6xl lg:text-9xl z-50">
+                Wolontariat
+              </h1>
+              <h2 className="flex font-inner text-main-color text-xl lg:text-4xl">
+                to nie tylko zajęcie, lecz chęć <br /> pomagania społeczeństwu
+              </h2>
+            </div>
+          </div>
+          <div className="flex absolute ml-64 mt-5 z-10 -rotate-[15deg] lg:relative lg:ml-0 lg:mt-0 lg:rotate-0">
+            <FaHandHoldingHeart className="flex text-8xl lg:text-[25rem] text-main-color"></FaHandHoldingHeart>
           </div>
         </div>
-        <div>
-          <FaHandHoldingHeart className="text-[25rem] text-main-color w-1/2 float-right"></FaHandHoldingHeart>
-        </div>
-        </div>
-        <div className="text-main-color text-7xl " >
-          <p>Jak ułatwimy Tobie <span className="text-main-color-2 font-bold">wolontariat</span></p>
+
+        <div className="text-main-color text-7xl ">
+          <p>
+            Jak ułatwimy Tobie{" "}
+            <span className="text-main-color-2 font-bold">wolontariat</span>
+          </p>
         </div>
         <div className="flex flex-row text-main-color gap-48 font-inner my-10">
           <div>
@@ -55,7 +59,6 @@ const Home: NextPage = () => {
               Nasza aplikacja jest intuicyjna i wygodna
             </p>
           </div>
-
         </div>
         <div className="my-10">
           <p className="text-main-color text-7xl">Jak to działa?</p>
@@ -64,25 +67,26 @@ const Home: NextPage = () => {
           <div>
             <p className="text-6xl font-regular">1</p>
             <p className="text-4xl">Stwórz konto</p>
-            <p className="text-lg font-regular">Stwórz konto na naszej stronie i uzupełnij swoje dane.</p>
+            <p className="text-lg font-regular">
+              Stwórz konto na naszej stronie i uzupełnij swoje dane.
+            </p>
           </div>
           <div>
             <p className="text-6xl font-regular">2</p>
             <p className="text-4xl">Wystaw się lub znajdź wolontariat</p>
-            <p className="text-lg font-regular">Zadeklaruj swoją chęć do udziału w wolontariacie poprzez wystawienie siebie lub znalezienie wolontariatu.</p>
+            <p className="text-lg font-regular">
+              Zadeklaruj swoją chęć do udziału w wolontariacie poprzez
+              wystawienie siebie lub znalezienie wolontariatu.
+            </p>
           </div>
           <div>
             <p className="text-6xl font-regular">3</p>
             <p className="text-4xl">Czekaj na odpowiedź i nieś dobro!</p>
-            <p className="text-lg font-regular">Znajdź odpowiadający Tobie wolontariat, czekaj aż ktoś się do Ciebie odezwie, dogadaj szczegóły i nieś dobro ludziom!</p>
+            <p className="text-lg font-regular">
+              Znajdź odpowiadający Tobie wolontariat, czekaj aż ktoś się do
+              Ciebie odezwie, dogadaj szczegóły i nieś dobro ludziom!
+            </p>
           </div>
-        </div>
-        <div className="font-inter flex w-screen h-[10rem] bg-main-color mt-24">
-          <p>
-            Hack Heroes competition project <br />
-            Made with heart by kluczi & youwish
-          </p> 
-          
         </div>
       </main>
     </>
